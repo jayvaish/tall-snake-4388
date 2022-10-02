13d244a9518be78570444c00b4ac1d2a0de92e50
ajay <vajaysingh2002@gmail.com>
2022-10-02 14:04:23 +0530

    Final Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Footer from './components/footer/Footer.jsx';
+
+function App() {
+  return (
+   <div><Footer/></div>
+  );
+}
+
+export default App;
